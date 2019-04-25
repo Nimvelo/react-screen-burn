ccd812b947a8af55ed4a35905e270e79e647ac55
Adam Bowles <adam.bowles@nimvelo.com>
2019-04-25 16:00:56 +0100

    Lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,42 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-// import postcss from 'rollup-plugin-postcss-modules'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+// import postcss from 'rollup-plugin-postcss-modules';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.tsx',
   output: [
     {
+      exports: 'named',
       file: pkg.main,
       format: 'cjs',
-      exports: 'named',
-      sourcemap: true
+      sourcemap: true,
     },
     {
+      exports: 'named',
       file: pkg.module,
       format: 'es',
-      exports: 'named',
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
   plugins: [
     external(),
     postcss({
-      modules: true
+      modules: true,
     }),
     url(),
     svgr(),
     resolve(),
     typescript({
+      clean: true,
       rollupCommonJSResolveHack: true,
-      clean: true
     }),
-    commonjs()
-  ]
-}
+    commonjs(),
+  ],
+};
