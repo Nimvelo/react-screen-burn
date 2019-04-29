9d4c41f459933e7492d7329b8e313f0e8d659b29
Adam Bowles <adam.bowles@nimvelo.com>
2019-04-29 16:42:41 +0100

    If supplied 0 for either trigger time, don't bother with a timeout

--- src/Line/component.tsx
+++ src/Line/component.tsx
@@ -5,13 +5,6 @@ import styles from './style.module.scss';
 import { Props, State } from './interfaces';
 
 export default class ScreenBurnLine extends React.Component<Props, State> {
-  public static defaultProps: Props = {
-    colors: ['#ff0000', '#00ff00', '#0000ff'],
-    retriggerTime:
-      process.env.NODE_ENV !== 'production' ? 1000 : 60 * 60 * 1000,
-    triggerTime: process.env.NODE_ENV !== 'production' ? 2000 : 60 * 60 * 2000,
-  };
-
   private determineInitialColor = (): React.CSSProperties['color'] => {
     let backgroundColor = '#ff0000';
     const deduplicatedColors = Array.from(new Set(this.props.colors));
@@ -23,65 +16,90 @@ export default class ScreenBurnLine extends React.Component<Props, State> {
     return backgroundColor;
   };
 
-  public readonly state: State = {
-    backgroundColor: this.determineInitialColor(),
-    lineSize: 1 / (window.devicePixelRatio || 1),
-    top: 0,
-    triggered: false,
-  };
+  private trigger = (): void => {
+    const updateTop = (): void => {
+      const step = this.state.lineSize;
+      const top = this.state.top + step;
 
-  public componentDidMount(): void {
-    setTimeout((): void => {
-      this.setState({
-        triggered: true,
-      });
-    }, this.props.triggerTime);
-  }
-
-  public componentDidUpdate(prevProps: Props, prevState: State): void {
-    if (prevState.triggered === false && this.state.triggered === true) {
-      const updateTop = (): void => {
-        const step = this.state.lineSize;
-        const top = this.state.top + step;
-
-        // Move the current line down
-        if (this.state.top < window.innerHeight - step) {
-          this.setState({
-            top,
-          });
-
-          window.requestAnimationFrame(updateTop);
-
-          return;
-        }
-
-        // Determine next line characteristics
-        const deduplicatedColors = Array.from(new Set(this.props.colors));
-        const currentIndex = deduplicatedColors.findIndex(
-          (color): boolean => color === this.state.backgroundColor,
-        );
-
-        let nextColor = '#ff0000';
-        if (deduplicatedColors.length) {
-          nextColor = deduplicatedColors[
-            (currentIndex + 1) % deduplicatedColors.length
-          ] as string;
-        }
-
+      // Move the current line down
+      if (this.state.top < window.innerHeight - step) {
         this.setState({
-          backgroundColor: nextColor,
-          top: 0,
-          triggered: false,
+          top,
         });
 
+        window.requestAnimationFrame(updateTop);
+
+        return;
+      }
+
+      // Determine next line characteristics
+      const deduplicatedColors = Array.from(new Set(this.props.colors));
+      const currentIndex = deduplicatedColors.findIndex(
+        (color): boolean => color === this.state.backgroundColor,
+      );
+
+      let nextColor = '#ff0000';
+      if (deduplicatedColors.length) {
+        nextColor = deduplicatedColors[
+          (currentIndex + 1) % deduplicatedColors.length
+        ] as string;
+      }
+
+      this.setState({
+        backgroundColor: nextColor,
+        top: 0,
+        triggered: false,
+      });
+
+      const retriggerTime = this.props.retriggerTime || 0;
+
+      if (retriggerTime > 0) {
         setTimeout((): void => {
           this.setState({
             triggered: true,
           });
         }, this.props.retriggerTime);
-      };
+      } else {
+        this.setState({
+          triggered: true,
+        });
+      }
+    };
 
-      window.requestAnimationFrame(updateTop);
+    window.requestAnimationFrame(updateTop);
+  };
+
+  public static defaultProps: Props = {
+    colors: ['#ff0000', '#00ff00', '#0000ff'],
+    retriggerTime:
+      process.env.NODE_ENV !== 'production' ? 1000 : 60 * 60 * 1000,
+    triggerTime: process.env.NODE_ENV !== 'production' ? 2000 : 60 * 60 * 2000,
+  };
+
+  public readonly state: State = {
+    backgroundColor: this.determineInitialColor(),
+    lineSize: 1 / (window.devicePixelRatio || 1),
+    top: 0,
+    triggered: this.props.triggerTime === 0,
+  };
+
+  public componentDidMount(): void {
+    const triggerTime = this.props.triggerTime || 0;
+
+    if (triggerTime > 0) {
+      setTimeout((): void => {
+        this.setState({
+          triggered: true,
+        });
+      }, triggerTime);
+    } else {
+      this.trigger();
+    }
+  }
+
+  public componentDidUpdate(prevProps: Props, prevState: State): void {
+    if (prevState.triggered === false && this.state.triggered === true) {
+      this.trigger();
     }
   }
 
